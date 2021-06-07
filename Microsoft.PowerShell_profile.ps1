6f099dae215c72621f86611db96e17e0e83f8b19
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-06-07 13:59:18 +0800

    [minor change]: only convert the drive letter to lowercase

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,5 @@
 $e = [char]0x1B
 
 function Prompt {
-    "${e}[1;36m{0}@{1}${e}[0m:${e}[1;34m/mnt/{2}${e}[0m$ " -f [System.Environment]::UserName,($env:COMPUTERNAME).ToLower(),(Get-Location).ToString().ToLower().Replace(":", "").Replace("\", "/")
+    "${e}[1;36m{0}@{1}${e}[0m:${e}[1;34m/mnt/{2}${e}[0m$ " -f [System.Environment]::UserName,($env:COMPUTERNAME).ToLower(),((Get-Location).ToString().Replace(":", "").Replace("\", "/") -replace "^[A-Z]", (Get-Location).ToString().Replace(":", "").Replace("\", "/")[0].ToString().ToLower())
 }
